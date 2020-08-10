6f22104917b9f545d2b3d312e6fe8be51655c0fe
tamaspal2020 <tamaspal@gmail.com>
2020-08-10 08:03:36 +0200

    x

--- OOP/src/gamepc/CentralControl.java
+++ OOP/src/gamepc/CentralControl.java
@@ -4,6 +4,7 @@ import gamepc.Computer;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 import books.Main.genre;
 public class CentralControl {
@@ -15,7 +16,7 @@ static int[] magszam= {1,2,4,8,16};
 	public static void main(String[] args) {
 		Room r = new Room();
 		r.szabadgepek=roomFeltolt();
-		kiirGepek(r);
+		gui(r);
 		
 	}
 public static  ArrayList<Computer> roomFeltolt() {
@@ -36,7 +37,9 @@ public static  void kiirGepek(Room r){
 	System.out.println("A szabad gépek listája: ");
 	for(int i=0; i<r.szabadgepek.size(); i++) 
 	{
+		System.out.println(i+1);
 		egyGepkiir(r.szabadgepek.get(i));
+		System.out.println("------------------------------------------------");
 	} 
 	System.out.println("A foglalt gépek listája: ");
 	for(int i=0; i<r.foglaltgepek.size(); i++) 
@@ -57,24 +60,42 @@ public static void egyGepkiir(Computer c) {
 	System.out.println("Processzor: "+c.proc);
 	System.out.println("Magok száma: "+c.magszam);
 	System.out.println("Bérlés díja óránként: "+c.oradij);
+	System.out.println("Bérlés kezdete"+c.berleskezdete);
+	System.out.println("Bérlés vége"+c.berlesvege);
 }
-public static void gui() {
+public static void gui(Room r) {
 	int valaszt=-1;
+	Scanner sc=new Scanner(System.in);
 	while(valaszt!=0) {
 		System.out.println("Foglalás - 1");
 		System.out.println("Gép adatok - 2");
 		System.out.println("Kilépés - 0"); 
+		valaszt=sc.nextInt();
 		switch  (valaszt) {
 			case 1:
+				foglalas(r);
 				break;
 			case 2:
+				
+				kiirGepek(r);
 				break;
 			default:
 			break;
-		}					
+		}	
 	}
+	sc.close();
 }
-public void foglalas() {
-	
+public static void foglalas(Room r) {
+	System.out.println("Adja meg a gép sorszámát:");
+	Scanner sc=new Scanner(System.in);
+	int sorsz=sc.nextInt()-1;	
+	System.out.println("Adja meg, mikortól szeretné lefoglalni (8-20): ");
+	Scanner sc2=new Scanner(System.in);
+	int tol=sc.nextInt();
+	System.out.println("Adja meg, hány órára szeretné lefoglalni:");
+	Scanner sc3=new Scanner(System.in);
+	int orakszama=sc.nextInt();
+	r.szabadgepek.get(sorsz).berleskezdete=tol;
+	r.szabadgepek.get(sorsz).berlesvege=tol+orakszama;
 }
 }
