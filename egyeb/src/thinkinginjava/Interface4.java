d801bb107603e94990df99c34fffd303ef724e6d
tamaspal2020 <tamaspal@gmail.com>
2020-08-11 05:52:58 +0200

    x

---
+++ egyeb/src/thinkinginjava/Interface4.java
@@ -0,0 +1,9 @@
+package thinkinginjava;
+import thinkinginjava.Interface1;
+import thinkinginjava.Interface2;
+import thinkinginjava.Interface3;
+
+public interface Interface4 extends Interface1, Interface2,Interface3   {
+ void addtwonumbers(int a, int b);
+	
+}
