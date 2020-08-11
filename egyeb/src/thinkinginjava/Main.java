a04ce59c0c807b329628ad5176d172391e4f26af
tamaspal2020 <tamaspal@gmail.com>
2020-08-11 05:52:47 +0200

    x

---
+++ egyeb/src/thinkinginjava/Main.java
@@ -0,0 +1,15 @@
+package thinkinginjava;
+import thinkinginjava.ChildCl;
+public class Main {
+
+	public static void main(String[] args) {
+		ChildCl c=new ChildCl();
+		kiir(c);
+
+	}
+	
+	public static void kiir(Interface1 i) {
+		i.say();
+	}
+
+}
