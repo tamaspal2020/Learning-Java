4f28f3fd8b18557eb72a4455c7a723332df47ff5
tamaspal2020 <tamaspal@gmail.com>
2020-08-14 07:00:49 +0200

    x

--- egyeb/src/absterface/Center.java
+++ egyeb/src/absterface/Center.java
@@ -7,6 +7,12 @@ public class Center {
 	public static void main(String[] args) {
 		Am15 a=new Am15();
 		Gx123 g=new Gx123();
+		fire(a);
+		fire(g);
 	}
-
+	
+public static void fire(Gun g) {
+	g.go();
+}
+	
 }
