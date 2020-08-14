b3a61e718fc09653db199c1b0e566d5b34812d1c
tamaspal2020 <tamaspal@gmail.com>
2020-08-14 07:01:18 +0200

    x

---
+++ egyeb/src/thinkinginjava/ChildCl.java
@@ -0,0 +1,14 @@
+package thinkinginjava;
+
+public class ChildCl extends ParentCl implements Interface4{
+	public void say() {}
+	public void draw() {
+		System.out.println("**************");
+		
+	}
+	public void write() {}
+	public void putonconsole(){} 
+	public void print(){} 
+	public void numbers(){} 
+	public void addtwonumbers(int a, int b) {}
+}
