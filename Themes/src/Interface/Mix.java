302350cf05851779f93b828e8a8f57e17d4d50a0
tamaspal2020 <tamaspal@gmail.com>
2020-08-18 12:48:07 +0200

    c

---
+++ Themes/src/Interface/Mix.java
@@ -0,0 +1,21 @@
+package Interface;
+
+public class Mix {
+
+}
+
+abstract class Abstr1{
+	
+}
+interface Inter1{
+	
+}
+interface Inter2 extends Inter1{
+	
+}
+abstract class Abstra1 implements Inter1{
+	
+}
+abstract class Abstra2 extends Abstra1 implements Inter1{
+	
+} 
