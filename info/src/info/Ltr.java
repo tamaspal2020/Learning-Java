916ce471b243422253bb718333cf45ce9802cebb
tamaspal2020 <tamaspal@gmail.com>
2020-08-20 06:55:58 +0200

    c

--- info/src/info/Ltr.java
+++ info/src/info/Ltr.java
@@ -1,11 +1,57 @@
 package info;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ltr {  
 public static void main(String[] args) {  
-    int i=1;  
-    while(i<=10){  
-        System.out.println(i);  
-    i++;  
-    }  
+	Synth<Instrument> sy=new Synth<Instrument>();
+	
+	SoundType<Piano> pt=new SoundType<Piano>();
+	Piano p=new Piano();
+	pt.add(p);
+		
+	SoundType<Trumpet> tt=new SoundType<Trumpet>();
+	Trumpet t=new Trumpet();
+	tt.add(t);
+	
+	sy.addToList(tt);
+	sy.addToList(pt);
 }  
 }  
+
+class Synth<T extends Instrument>{
+	List<Instrument> inlist=new ArrayList<>();
+	public void sound(T p) {
+		p.sound();
+	}
+	public void addToList(SoundType<? extends Instrument> st) {
+		inlist.add(st.i);
+	}
+}
+interface Instrument{
+	void sound();
+}
+
+class Trumpet implements Instrument{
+	public void sound() {
+		System.out.println("Sounds like a trumpet");
+	}
+}
+class Piano implements Instrument{
+public void sound() {
+		System.out.println("Sounds like piano");
+	}
+}
+class Violin{
+public void sound() {
+		System.out.println("Sounds like violin");
+	}
+}
+class SoundType<T>{
+	public T i;
+	public void add(T i) {
+		this.i=i;
+	}
+}
