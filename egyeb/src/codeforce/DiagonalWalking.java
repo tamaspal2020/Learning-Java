e15578b1f4a81af9bba7bec47f210a494ac24c1c
palt <palt@IT-1115.si.net>
2020-08-26 16:06:55 +0200

    c

--- egyeb/src/codeforce/DiagonalWalking.java
+++ egyeb/src/codeforce/DiagonalWalking.java
@@ -1,10 +1,16 @@
 package codeforce;
 
+import java.util.Scanner;
 public class DiagonalWalking {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	char[] s=sc.next().toCharArray();	
+	for(int i=0; i<n-1; i++) {
+		if (s[i]!=s[i+1]) {
+			n--;
+		}
 	}
-
+	System.out.println(n);
+}
 }
