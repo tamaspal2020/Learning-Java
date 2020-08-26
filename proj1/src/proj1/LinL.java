1c90a83902e31ac5f0f2bf2bee8e9eb87c4e96b1
palt <palt@IT-1115.si.net>
2020-08-26 15:23:23 +0200

    c

--- proj1/src/proj1/LinL.java
+++ proj1/src/proj1/LinL.java
@@ -3,8 +3,50 @@ package proj1;
 public class LinL {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		LinL linkedlist1=new LinL();
+		linkedlist1.addNewNode(10);
+		linkedlist1.addNewNode(234);
+		linkedlist1.addNewNode(-23423);
+		linkedlist1.printLinl();
 	}
+	
+	class Node{
+		int data;
+		Node next;
+		
+		public Node(int data) {
+			this.data=data;
+			this.next=null;
+		}
+	}
+	public Node head=null;
+	public Node tail=null;
+		public void  addNewNode(int data){
+			Node newNode=new Node(data);
+		
+			if(head==null) {
+				head=newNode;
+				tail=newNode;
+			}
+			else {
+				tail.next=newNode;
+				tail=newNode;
+			}
+		}
+		public void printLinl() {
+			Node actual=head;
+			if(head==null) {
+				System.out.println("Empty");
+			}
+			else{
+				while(actual!=null) {
+					System.out.println(actual.data);
+					actual=actual.next;
+				}
+			}
+		}
+		
+	}
+	
 
-}
+
