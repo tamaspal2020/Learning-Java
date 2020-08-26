5c5ba01904d1ed4675f8ddcc61f9bea2aa676778
palt <palt@IT-1115.si.net>
2020-08-26 15:26:41 +0200

    c

---
+++ proj1/src/proj1/DiagonalWalking.java
@@ -0,0 +1,16 @@
+package proj1;
+
+import java.util.Scanner;
+public class DiagonalWalking {
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	char[] s=sc.next().toCharArray();	
+	for(int i=0; i<n-1; i++) {
+		if (s[i]!=s[i+1]) {
+			n--;
+		}
+	}
+	System.out.println(n);
+}
+}
