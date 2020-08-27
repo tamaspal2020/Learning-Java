80805e7cbb83d5f094b0d59ccfb77801445a557c
tamaspal2020 <tamaspal@gmail.com>
2020-08-27 08:10:15 +0200

    Fract

--- egyeb/src/egyeb/AddFractions.java
+++ egyeb/src/egyeb/AddFractions.java
@@ -1,10 +1,20 @@
 package egyeb;
-
+import java.util.Scanner; 
 public class AddFractions {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		Scanner sc=new Scanner(System.in);
+		for(int i=0; i<4; i++){
+			}
+		}
 }
+
+class Fract{
+	public boolean recognizeFract(String s) {
+		boolean fract=false;
+		
+		Set<>
+		return fract;
+	}
+}
+
