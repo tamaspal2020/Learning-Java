b8bdda837cb5a2f8b229ef05c06d2f41591d247f
tamaspal2020 <tamaspal@gmail.com>
2020-08-29 11:11:07 +0200

    ropewalkers

---
+++ egyeb/src/codeforce/Ropewalkers.java
@@ -0,0 +1,16 @@
+package codeforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ropewalkers {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st= new StringTokenizer(br.readLine());
+
+	}
+
+}
