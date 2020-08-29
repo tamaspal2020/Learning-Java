dd3d574b163cd49fb994298334d44a84906da85f
tamaspal2020 <tamaspal@gmail.com>
2020-08-29 11:10:49 +0200

    treesetdemo

---
+++ Themes/src/collections/TreesetDemo.java
@@ -0,0 +1,18 @@
+package collections;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TreesetDemo {
+
+	public static void main(String[] args) {
+		Set<String> tres=new TreeSet<>();
+		tres.add("nnnnn");
+		tres.add("aaaa");
+		tres.add("mmmm");
+		tres.add("dddd");
+		tres.add("tzdfsg");
+		System.out.println(tres);
+	}
+}
