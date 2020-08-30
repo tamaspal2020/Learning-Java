11006652ddd0f1ec11daf0526a3a148e349ac73c
tamaspal2020 <tamaspal@gmail.com>
2020-08-30 05:02:26 +0200

    classes

--- egyeb/src/egyeb/Classes.java
+++ egyeb/src/egyeb/Classes.java
@@ -1,17 +1,33 @@
 package egyeb;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
 public class Classes {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		B b=new B();
 		b.otherclass.kiir(200);
+		Set<Droid> ds=new HashSet<>();
+		ds.add(new Droid(1L, "ADS"));
+		ds.add(new Droid(2L, "DGH"));
+		ds.add(new Droid(3L, "ADDFGH"));
+		ds.add(new Droid(4L, "HGJ"));
+		System.out.println(ds.size());
+		
 	}
 
 }
 
 class A{
+int c;	
 public A() {
+	
+}	
+public A(int x) {
 	
 }	
 void kiir(int i){
@@ -21,9 +37,10 @@ void kiir(int i){
 
 class B{
 	public A otherclass=new A();
+	List<A> alist =new LinkedList<>();
 	void create() {
 	for (int i=0; i<10; i++) {
-		
+		alist.add(new A());
 	}
 	}
 }
