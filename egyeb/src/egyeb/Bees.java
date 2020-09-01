8fc3ea781773a4f75065cefc911247cc9c80c03e
tamaspal2020 <tamaspal@gmail.com>
2020-09-01 20:21:37 +0200

    dg

---
+++ egyeb/src/egyeb/Bees.java
@@ -0,0 +1,11 @@
+package egyeb;
+//flowers in circles arond the hive different distances, differenct colors
+///  on bee collect only one color, and fly only max d distance from the hive
+public class Bees {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
