226dc71d5575f76621b15cc8e2a75ffb4795fff5
tamaspal2020 <tamaspal@gmail.com>
2020-09-02 13:43:30 +0200

    b

--- egyeb/src/egyeb/Bees.java
+++ egyeb/src/egyeb/Bees.java
@@ -1,14 +1,15 @@
 package egyeb;
 
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-impo
+
 //flowers in circles arond the hive different distances, differenct colors
 ///  on bee collect only one color, and fly only max d distance from the hive
 public class Bees {
 
 	public static void main(String[] args) {
-	   BufferedReader br=new BufferedReader(new InputStreamReader(System.in); 	
+	   BufferedReader br=new BufferedReader(new InputStreamReader(System.in)); 	
 
 	}
 
