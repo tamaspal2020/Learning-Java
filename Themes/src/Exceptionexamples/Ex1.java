67af5826e5eb0f8161961a06d73d739cd63735d8
tamaspal2020 <tamaspal@gmail.com>
2020-09-04 13:26:52 +0200

    exceptionhandling

--- Themes/src/Exceptionexamples/Ex1.java
+++ Themes/src/Exceptionexamples/Ex1.java
@@ -2,7 +2,7 @@ package Exceptionexamples;
 
 public class Ex1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws nemParosException {
 		try{
 			int a=0;
 			int b=10;
@@ -12,6 +12,7 @@ public class Ex1 {
 			System.out.println("Division by zero");
 		}
 		System.out.println("After the code");
+		intcontrol(3);
 		try {
 			intcontrol(3);
 		}
@@ -23,12 +24,12 @@ public class Ex1 {
 	}
 	public static void intcontrol(int a) throws nemParosException {
 		if(a%2!=0) {
-			throw new nemParosException("fg"); 
+			throw new nemParosException("This is the text of the exception itself,  if this exception is not catched, the program will stop here"); 
 		}
 	}
 }
 class nemParosException extends Exception{
 	nemParosException(String etext){
-		///super(etext);
+		super(etext);
 	}
 }
