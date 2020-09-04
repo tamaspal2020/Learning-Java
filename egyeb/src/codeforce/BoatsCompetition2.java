b65d3a90ede41d31d266ab4b14eb0de4ad4fb2b7
tamaspal2020 <tamaspal@gmail.com>
2020-09-04 13:58:54 +0200

    boatcompetition

--- egyeb/src/codeforce/BoatsCompetition2.java
+++ egyeb/src/codeforce/BoatsCompetition2.java
@@ -1,6 +1,10 @@
 package codeforce;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Scanner;
 
 public class BoatsCompetition2 {
@@ -10,15 +14,49 @@ public class BoatsCompetition2 {
 		Scanner sc=new Scanner(System.in);
 		int tc=sc.nextInt();
 		int[] result=new int[tc];
+		Map<Integer, HashSet<Integer>> possibleteams =new HashMap<>();
 		LinkedList<Integer> sulyok=new LinkedList<>();
+		
+		
 		for(int i=0; i<tc; i++) {
-			sulyok.clear();		
+			sulyok.clear();	
+			possibleteams.clear();
 			int n=sc.nextInt();
+			if(n==1 ) {
+				System.out.println(1);
+			}
+			else {
 			for(int j=0;j<n;j++) {
 				sulyok.add(sc.nextInt());
 			}
+		
+			Collections.sort(sulyok);
+			int smallestweight=sulyok.get(0)+sulyok.get(1);
+			int biggestweight=sulyok.get(sulyok.size()-1)+sulyok.get(sulyok.size()-2);			
+			System.out.println(smallestweight+"+"+biggestweight);
+			for(int w=smallestweight; w<=biggestweight; w++) {
+				possibleteams.put(w, pairswithWweight(w,sulyok));
+			}
+		}
 			
 		}
+		System.out.println(possibleteams);
+	}
+
+	public static HashSet<Integer> pairswithWweight(int w, LinkedList<Integer> sortedl) {
+		HashSet<Integer> teams=new HashSet<Integer>();
+		for(int i=0; i<sortedl.size()-1; i++) {
+			for(int j=i+1; j<sortedl.size(); j++) {
+				if( (sortedl.get(i)+sortedl.get(j))==w ) {
+					if( !(teams.contains(sortedl.get(i)))  &&   !(teams.contains(sortedl.get(i)))    )
+							{
+						teams.add(sortedl.get(i));
+						teams.add(sortedl.get(j));
+							}
+				}
+			}
+		}
+		return teams;
 	}
 
 	}
