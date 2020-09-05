18b8f315d6e3f708d7c0edcc4e0b5969a337f113
tamaspal2020 <tamaspal@gmail.com>
2020-09-05 06:14:16 +0200

    x

--- egyeb/src/codeforce/BoatCompetition3.java
+++ egyeb/src/codeforce/BoatCompetition3.java
@@ -1,9 +1,7 @@
 package codeforce;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.Map;
 import java.util.Scanner;
 
 public class BoatCompetition3 {
@@ -11,84 +9,48 @@ public class BoatCompetition3 {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		int tc=sc.nextInt();
-		int[] result=new int[tc];		
-		for(int i=0; i<tc; i++) {			
+
+		while(tc-- > 0) {
 			int n=sc.nextInt();
-			if(n==1 ) {
-				int x=sc.nextInt();				
-				result[i]=0;
+			int[] tomb = new int[n];
+			int max = Integer.MIN_VALUE;
+			for (int i = 0; i < n; i++) {
+				tomb[i] = sc.nextInt();    
+				max = Math.max(max, tomb[i]);///a legnagyobb elem
 			}
-			else {
-				if(n==2)
-				{					
-					int y=sc.nextInt();
-					y=sc.nextInt();
-					result[i]=1;
-				}
-				else {
-					int[] w=new int[n];
-					int maxsuly=0;
-					int minsuly=51;
-					for(int j=0;j<n;j++) {				
-						w[j]=sc.nextInt();
-						if(w[j]>maxsuly) {
-							maxsuly=w[j];
-						}
-						if(w[j]<minsuly) {
-							minsuly=w[j];
-						}
-			}
-					for(int f=0; f<w.length; f++) {
-						System.out.println(w[f]);
-					}
-			 maxsuly=maxsuly*2;
-			int teams=n/2;
-			minsuly=minsuly*2;
-			System.out.println(maxsuly+","+teams);
-			ArrayList<Integer> members=new ArrayList<Integer>();
-			ArrayList<Integer> usedindex=new ArrayList<>();
-			int suly=1;
-			boolean ok=false;
-				while(!ok) {
-					ok=false;
-					usedindex.clear();
-					members.clear();
-					System.out.println("teams"+teams);
-					
-					for(int x=0; x<w.length-1;x++) {
-						for (int y=x+1; y<w.length; y++) {
-							if(  !usedindex.contains(x)   && !usedindex.contains(y) ) {
-								System.out.println("minsuly"+minsuly+"w[x]"+w[x]+"w[y]"+w[y]);								
-								if(w[x]+w[y]==minsuly) {								
-									System.out.println(w[x]+","+w[y]+","+minsuly);
-									usedindex.add(x);
-									usedindex.add(y);
-									members.add(w[x]);
-									members.add(w[y]);
-									System.out.println("memberssisez"+members.size());
-								}
-							}
-							
-						}
-						System.out.println(members.size()+"---"+teams);
-					}
-					System.out.println(minsuly+","+members);
-					System.out.println(usedindex);
-					if (members.size()/2==teams) ok=true;
-					teams--;
-					minsuly++;
-					
-				}
-				result[i]=teams;
-				}
-			
-			}
+			Arrays.sort(tomb);
+			System.out.println(maxTeams(tomb,n));
 		}
-		for(int i=0; i<result.length; i++) {
-			System.out.println(result[i]);
-		}
-		sc.close();
-
 	}
-
+public static int maxTeams(int[] t , int n) {
+		int max=0;
+		
+		for(int sulyosszeg=2;sulyosszeg<=100; sulyosszeg++) {
+		
+			boolean[] indexes= new boolean[n+1];
+			int teams=0;
+			for(int x=0; x<n-1;x++)
+			{
+				for(int y=x+1; y<n;y++)
+				{
+					if(indexes[y]) {
+						continue;
+					}
+					if(indexes[x]) {
+						break;
+					}
+						
+						if(t[x]+t[y]==sulyosszeg) {
+							teams++;
+							indexes[x]=true;
+							indexes[y]=true;
+							max = Math.max(max, teams);
+							break;
+						}
+					}
+				}
+			}
+		
+		return max;
+	}
 }
