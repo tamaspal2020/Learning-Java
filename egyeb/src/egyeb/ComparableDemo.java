42831c19ca416131bd96ab9c0d5f2a2bc1d25ac5
tamaspal2020 <tamaspal@gmail.com>
2020-09-05 06:51:53 +0200

    comparable

--- egyeb/src/egyeb/ComparableDemo.java
+++ egyeb/src/egyeb/ComparableDemo.java
@@ -3,9 +3,11 @@ package egyeb;
 import java.util.HashSet;
 import java.util.TreeMap;
 
+import egyeb.Disk.ExistingIdException;
+
 public class ComparableDemo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ExistingIdException {
 		TreeMap<Disk, Integer> disklist=new TreeMap<>();
 		Disk d1=new Disk(1);
 		Disk d2=new Disk(2);
@@ -13,29 +15,32 @@ public class ComparableDemo {
 	}
 
 }
-class Disk implements Comparable{
+class Disk implements Comparable<Disk>{
 	private int id;
 	private static HashSet<Integer> idlist=new HashSet<>();
 	@Override
-	public int compareTo(Object o) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int compareTo(Disk d) {
+		if (this.id==d.id) return 0;
+		if (this.id<d.id) return -1;
+		return 1;
 	}
 	Disk(int id) throws ExistingIdException {
 		try {
-			if(idlist.contains(id) {
+			if(idlist.contains(id)) {
 				throw new ExistingIdException("Az id már létezik");
 			} 
-			this.id=id;
+			else {
+				idlist.add(id);
+				this.id=id;
+			}
 		}
 		catch(ExistingIdException e) {
-			System.out
+			System.out.println("Ilyen id már létezik");
 		}
 	}
 	class ExistingIdException extends Exception{
 		ExistingIdException(String text){
 			super(text);
 		}
-	}
-	
+	}	
 }
