865d64d3925aa6342a2c1277dc03785caca8e7ed
tamaspal2020 <tamaspal@gmail.com>
2020-09-05 06:32:31 +0200

    e

--- egyeb/src/egyeb/ComparableDemo.java
+++ egyeb/src/egyeb/ComparableDemo.java
@@ -1,25 +1,30 @@
 package egyeb;
 
+import java.util.HashSet;
 import java.util.TreeMap;
 
 public class ComparableDemo {
 
 	public static void main(String[] args) {
 		TreeMap<Disk, Integer> disklist=new TreeMap<>();
-		Disk d1=new Disk();
-		Disk d2=new Disk();
+		Disk d1=new Disk(1);
+		Disk d2=new Disk(2);
 		disklist.put(d1, 1);
 	}
 
 }
 class Disk implements Comparable{
-	private int id
+	private int id;
+	private static HashSet<Integer> idlist=new HashSet<>();
 	@Override
 	public int compareTo(Object o) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 	Disk(int id){
+		
+	}
+	class ExistingIdException extends Exception{
 		
 	}
 	
