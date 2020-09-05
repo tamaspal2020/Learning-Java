7b0b344a043442e3c2e985370b702e632633be69
tamaspal2020 <tamaspal@gmail.com>
2020-09-05 07:23:21 +0200

    compar

--- egyeb/src/egyeb/ComparableDemo.java
+++ egyeb/src/egyeb/ComparableDemo.java
@@ -2,21 +2,44 @@ package egyeb;
 
 import java.util.HashSet;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
-import egyeb.Disk.ExistingIdException;
+//import egyeb.Disk.ExistingIdException;
 
 public class ComparableDemo {
 
 	public static void main(String[] args) throws ExistingIdException {
-		TreeMap<Disk, Integer> disklist=new TreeMap<>();
+		TreeMap<Disk, TreeSet<Film>> disklist=new TreeMap<>();
+		
 		Disk d1=new Disk(1);
 		Disk d2=new Disk(2);
-		disklist.put(d1, 1);
+		Film f1=new Film(1);
+		Film f2=new Film(2);
+		Film f3=new Film(3);
+		TreeSet<Film> filmlist=new TreeSet<>();
+		filmlist.add(f2);
+		filmlist.add(f1);
+		TreeSet<Film> filmlist2=new TreeSet<>();
+		filmlist2.add(f3);
+		filmlist2.add(f2);
+		filmlist2.add(f1);
+		disklist.put(d2, filmlist2);
+		disklist.put(d1, filmlist);
+		for(Disk d : disklist.keySet()) {
+			System.out.println(d.getId()+":");
+			for(Film f : disklist.get(d)) {
+				System.out.println(f.getId());
+			}
+			System.out.println("--------------");
+		}
 	}
 
 }
 class Disk implements Comparable<Disk>{
 	private int id;
+	public int getId() {
+		return id;
+	}
 	private static HashSet<Integer> idlist=new HashSet<>();
 	@Override
 	public int compareTo(Disk d) {
@@ -38,9 +61,39 @@ class Disk implements Comparable<Disk>{
 			System.out.println("Ilyen id már létezik");
 		}
 	}
-	class ExistingIdException extends Exception{
-		ExistingIdException(String text){
-			super(text);
-		}
-	}	
+	
 }
+
+class Film implements Comparable<Film>{
+	private int id;
+	public int getId() {
+		return id;
+	}
+	private static HashSet<Integer> idlista=new HashSet<>();
+	public Film(int id) throws ExistingIdException{
+		try {
+			if(idlista.contains(id)) {
+				throw new ExistingIdException("Van már ilye id");
+			}
+			else {
+				this.id=id;
+				idlista.add(id);
+			}
+				
+		}
+		catch(ExistingIdException e) {
+			System.out.println("Ilyen id-vel létezik már film");
+		}
+	}
+	@Override
+	public int compareTo(Film f) {
+		if(this.id==f.id) return 0;
+		if(this.id<f.id) return -1;
+		return 1;
+	}
+}
+class ExistingIdException extends Exception{
+	ExistingIdException(String text){
+		super(text);
+	}
+}	
