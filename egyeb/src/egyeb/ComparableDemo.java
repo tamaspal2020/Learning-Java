32230e29e1ef8006b1b1a8cecdbb413e378905f2
tamaspal2020 <tamaspal@gmail.com>
2020-09-05 06:29:35 +0200

    disk

--- egyeb/src/egyeb/ComparableDemo.java
+++ egyeb/src/egyeb/ComparableDemo.java
@@ -6,10 +6,21 @@ public class ComparableDemo {
 
 	public static void main(String[] args) {
 		TreeMap<Disk, Integer> disklist=new TreeMap<>();
-
+		Disk d1=new Disk();
+		Disk d2=new Disk();
+		disklist.put(d1, 1);
 	}
 
 }
-class Disk{
+class Disk implements Comparable{
+	private int id
+	@Override
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	Disk(int id){
+		
+	}
 	
 }
