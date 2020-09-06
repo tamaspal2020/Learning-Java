7db68a7fb2d01cc07cd2289ec73fa02d53a811d7
tamaspal2020 <tamaspal@gmail.com>
2020-09-06 10:12:19 +0200

    sky

---
+++ egyeb/src/egyeb/Sky.java
@@ -0,0 +1,23 @@
+package egyeb;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Sky{
+	public static Set<Cloud> clouds =new TreeSet<>();
+	public static void main(String[] args) {
+		Cloud c1=new Cloud();
+		Cloud c2=new Cloud();
+		
+	}
+	public static Cloud mergeClouds(Cloud c1, Cloud c2) {
+		Cloud c3=
+		return c3;
+				
+	}
+}
+
+
+class Cloud{
+	
+}
