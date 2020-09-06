cc65bf8774648d4381fe93d533e052dae6bf3590
tamaspal2020 <tamaspal@gmail.com>
2020-09-06 10:29:04 +0200

    sky

--- egyeb/src/egyeb/Sky.java
+++ egyeb/src/egyeb/Sky.java
@@ -7,17 +7,24 @@ public class Sky{
 	public static Set<Cloud> clouds =new TreeSet<>();
 	public static void main(String[] args) {
 		Cloud c1=new Cloud();
-		Cloud c2=new Cloud();
-		
+		Cloud c2=new Cloud();				
 	}
 	public static Cloud mergeClouds(Cloud c1, Cloud c2) {
-		Cloud c3=
-		return c3;
-				
+		Cloud c3=new Cloud();
+		clouds.remove(c1);
+		clouds.remove(c2);
+		return c3;				
 	}
 }
 
-
 class Cloud{
-	
+	int latitude;
+	int longitude;
+	int height;
+	int watercontent;
+	int rainintensity;
+	int density;
+	public void rain(int time) {
+		watercontent=watercontent-time*rainintensity;
+	}
 }
