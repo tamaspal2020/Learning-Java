d2ff50085f8cdb8c1c3f56e8271c9f8c614afbdc
tamaspal2020 <tamaspal@gmail.com>
2020-09-08 06:23:10 +0200

    cheap travel codeforce

---
+++ egyeb/src/codeforce/T.java
@@ -0,0 +1,15 @@
+package codeforce;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class T {
+	public static void main(String[] args) throws IOException {		
+		    BufferedReader x = new BufferedReader(new InputStreamReader(System.in));
+	        String[] y=x.readLine().split(" ");
+	        int n=Integer.parseInt(y[0]);
+	        int m=Integer.parseInt(y[1]);
+	        int a=Integer.parseInt(y[2]);
+	        int b=Integer.parseInt(y[3]);					
+	        System.out.print((b/m>=a)?n*a:n<m?(b<n*a?b:a):n%m!=0?(b<a?(n/m)*b+b:(n/m)*b+a*(n%m)):(n/m)*b);
+	}
+}
