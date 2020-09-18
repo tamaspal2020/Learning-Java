9c81e4f5f922d6bc7928cd049d77fbc655749143
tamaspal2020 <tamaspal@gmail.com>
2020-09-18 17:47:07 +0200

    # WARNING: head commit changed in the meantime
    exception

---
+++ egyeb/src/codeforce/PINCodes3.java
@@ -0,0 +1,50 @@
+package codeforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.TreeSet;
+public class PINCodes3 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader x= new BufferedReader(new InputStreamReader(System.in));
+		int t=Integer.parseInt(x.readLine());		
+		for(int i=0; i<t; i++) {
+			int n=Integer.parseInt(x.readLine());		
+			int[][] k=new int[n][4];
+			TreeSet<Integer> l=new TreeSet<>();
+			for(int j=0; j<n; j++) {
+				 String y=x.readLine();				 
+				 k[j][0]=Integer.parseInt(String.valueOf(y.charAt(0)));
+				 l.add(k[j][0]);
+				 k[j][1]=Integer.parseInt(String.valueOf(y.charAt(1)));
+				 k[j][2]=Integer.parseInt(String.valueOf(y.charAt(2)));
+				 k[j][3]=Integer.parseInt(String.valueOf(y.charAt(3)));			 
+			}			
+			int count=0;
+			boolean[] visited=new boolean[n];
+			for(int v=0; v<n;v++) visited[v]=false;
+			for(int o=0; o<n-1; o++) {					
+				 for(int u=o+1; u<n; u++) {				 
+						 if ( (k[o][0]==k[u][0] &&  k[o][1]==k[u][1] && k[o][2]==k[u][2] &&  k[o][3]==k[u][3]) && !visited[u] ){
+							 	int digit=0;
+							 	while(l.contains(digit)) {
+							 		digit++;
+							 	}
+							 	l.add(digit);
+							 	k[o][0]=digit;
+							 	count++;
+							 	visited[o]=true;
+							 }
+							 
+					 }
+			}	
+			System.out.println(count);
+			for(int z=0; z<k.length;z++) {				
+				for(int j=0; j<4;j++) {
+					System.out.print(k[z][j]);
+				}
+				System.out.println();
+			}		
+	}
+}
+}
