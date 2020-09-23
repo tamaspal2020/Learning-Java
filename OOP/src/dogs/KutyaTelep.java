9dbfc21cc028762d184dd81cc648e2162444af29
tamaspal2020 <tamaspal@gmail.com>
2020-09-23 09:06:41 +0200

    dogs

---
+++ OOP/src/dogs/KutyaTelep.java
@@ -0,0 +1,21 @@
+package dogs;
+
+public class KutyaTelep {
+
+	public static void main(String[] args) {
+	    
+
+	}
+
+}
+class Dog{
+	
+}
+
+class Telep{
+	
+}
+
+enum faj{
+	terrier, bulldog, spaniel, vizsla;
+}
