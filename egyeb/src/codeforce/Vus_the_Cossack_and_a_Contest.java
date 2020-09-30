11a8d3db5ef6ae2c4d7e530de062ad677320e6fc
Tamás <Tams@192.168.0.234>
2020-09-30 13:30:18 +0200

    vus

--- egyeb/src/codeforce/Vus_the_Cossack_and_a_Contest.java
+++ egyeb/src/codeforce/Vus_the_Cossack_and_a_Contest.java
@@ -3,14 +3,11 @@ package codeforce;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
 public class Vus_the_Cossack_and_a_Contest {
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader x = new BufferedReader(new InputStreamReader(System.in));
         String[] y=x.readLine().split(" ");
         System.out.println(((Integer.parseInt(y[1])>=Integer.parseInt(y[0]))&&(Integer.parseInt(y[2])>=Integer.parseInt(y[0])))?"Yes":"No");
-
 	}
 
 }
