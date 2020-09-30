0f9c7fd9303301cab5a7cfc51b6348ab17576ee3
Tamás <Tams@192.168.0.234>
2020-09-30 13:38:56 +0200

    privateclass

--- egyeb/src/egyeb/ZXC.java
+++ egyeb/src/egyeb/ZXC.java
@@ -3,15 +3,21 @@ package egyeb;
 public class ZXC {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		TestCl a=new TestCl();
+		a.
 	}
 
 }
  class TestCl{
 	
-	private class InnterClass{
-		
+	private class InnerClass1{
+		private InnerClass1(int i) {
+			System.out.println(i+"...");
+		}
+	}
+	public void Instantiater() {
+		InnerClass1 i=new InnerClass1(2);
+		InnerClass1 i2=new InnerClass1(33);
 	}
 	
 }
