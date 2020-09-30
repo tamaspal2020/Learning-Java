43cfc7a0bbb8c91b113bfbfb48926dc9be04a9ee
Tamás <Tams@192.168.0.234>
2020-09-30 13:36:40 +0200

    privatecl

---
+++ egyeb/src/egyeb/ZXC.java
@@ -0,0 +1,17 @@
+package egyeb;
+
+public class ZXC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+ class TestCl{
+	
+	private class InnterClass{
+		
+	}
+	
+}
