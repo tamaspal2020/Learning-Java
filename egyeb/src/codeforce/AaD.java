b5f1ff64442e77bf9d0d521fe3edb3e640400068
tamaspal2020 <tamaspal@gmail.com>
2020-10-02 11:49:23 +0200

    Anton and Digits codeforces

---
+++ egyeb/src/codeforce/AaD.java
@@ -0,0 +1,16 @@
+package codeforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class AaD {
+	public static void main(String[] args) throws IOException {
+		BufferedReader x = new BufferedReader(new InputStreamReader(System.in));
+        String[] y=x.readLine().split(" ");
+        int k2=Integer.parseInt(y[0]);
+        int k3=Integer.parseInt(y[1]);
+        int k5=Integer.parseInt(y[2]);
+        int k6=Integer.parseInt(y[3]);
+        System.out.println(Math.min(k6,Math.min(k2,k5))*256 +Math.min(k3, k2-Math.min(k6,Math.min(k2,k5)))*32);        
+	}
+}
